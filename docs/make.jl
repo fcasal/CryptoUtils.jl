53a0a8ea4cba7c5ba1ca876961d98c32fbc9ded8
fcasal <fcasal@users.noreply.github.com>
2019-07-22 01:43:07 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,7 @@
 using Documenter, CryptoUtils
 
-makedocs(;
-    modules=[CryptoUtils],
-    format=Documenter.HTML(),
-    pages=[
-        "Home" => "index.md",
-    ],
-    repo="https://github.com/fcasal/CryptoUtils.jl/blob/{commit}{path}#L{line}",
-    sitename="CryptoUtils.jl",
-    authors="fcasal",
-    assets=String[],
+makedocs(modules = [CryptoUtils], sitename = "CryptoUtils.jl")
+
+deploydocs(
+    repo = "github.com/fcasal/CryptoUtils.jl.git",
 )
